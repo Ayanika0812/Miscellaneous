a7b0a136f4c3996dc5cbb892eceba55e9674dfe8
Ayanika Paul <147071109+Ayanika0812@users.noreply.github.com>
2025-06-11 15:48:13 +0530

    Create Search_Insert_position.java
    11/06/2025

---
+++ easy/Search_Insert_position.java
@@ -0,0 +1,22 @@
+/*Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.*/
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        //for o(log n)  = binary search
+        int start =0 ,end=nums.length - 1;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                end=mid-1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return start;  //insert loc
+    }
+}
