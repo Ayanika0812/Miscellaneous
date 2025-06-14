93a8210ae302771184695ad93e59b25197a6cb11
Ayanika Paul <147071109+Ayanika0812@users.noreply.github.com>
2025-06-14 20:39:49 +0530

    Update Majority_element.java

--- easy/Majority_element.java
+++ easy/Majority_element.java
@@ -24,3 +24,34 @@ class Solution {
         return -1;
     }
 }
+
+
+//Boyer Moore -> OPTIMAL
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count =0;
+        int ele = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(count == 0){
+                count = 1;
+                ele = nums[i];
+            }
+            else if(nums[i] == ele){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        int count1 =0 ;
+        for(int i =0 ;i<nums.length;i++){
+            if(nums[i] == ele) count1++;
+        }
+
+        if(count1 > (nums.length / 2)){
+            return ele;
+        }
+        return -1;
+    }
+}
