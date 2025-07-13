5c366fec4a71738f83be9a1abc5429aafdcba958
Ayanika Paul <147071109+Ayanika0812@users.noreply.github.com>
2025-07-14 00:24:00 +0530

    13/07/25

--- easy/ContainsDuplicate.java
+++ easy/ContainsDuplicate.java
@@ -32,3 +32,22 @@ class Solution {
         return false;
     }
 }
+
+
+//BETTER
+
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+}
