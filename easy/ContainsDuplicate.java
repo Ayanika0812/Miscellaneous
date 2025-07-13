1dd4b6bfffff47ed519a75f90b1f6a7dcd266bfa
Ayanika Paul <147071109+Ayanika0812@users.noreply.github.com>
2025-07-13 23:36:03 +0530

    13/07/25

---
+++ easy/ContainsDuplicate.java
@@ -0,0 +1,34 @@
+/*
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+Explanation:
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+Explanation:
+All elements are distinct.
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+*/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int num : nums){
+            if(map.containsKey(num)){
+                return true;
+            }
+            else{
+                map.put(num,1);
+            }
+        }
+        return false;
+    }
+}
